6f682dafca2f27881eca497427bc439f4ab35df9
Mikko Ilmari Nummelin <mnummeli@gmail.com>
2017-10-29 21:26:38 +0200

    3. sivu

--- 7B/app.js
+++ 7B/app.js
@@ -1,6 +1,13 @@
 $(function () {
     var otsikot = ['Kissat', 'Matkailu', 'Tietokoneet'];
 
+    for (var i = 0; i < 3; i++) {
+        $('nav.top').append('<button id="b' + (i + 1) + '">' +
+                otsikot[i] +
+                '</button>');
+    }
+    $('nav.top').append('<div class="clearfix"></div>');
+
     $('nav.top button').click(function (e) {
         var page = e.target.id[1];
         location.hash = '#' + page;
