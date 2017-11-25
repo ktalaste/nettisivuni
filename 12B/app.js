30aaf7a92333bc10f33b111bf35f032c8c5fb465
Mikko Ilmari Nummelin <mnummeli@gmail.com>
2017-11-25 20:48:47 +0200

    Lisäys toimii radiobuttoneita lukuun ottamatta.

--- 12B/app.js
+++ 12B/app.js
@@ -1,4 +1,28 @@
 $(function() {
+  var henkilot = [
+    {
+      etunimi: "Aku",
+      sukunimi: "Ankka",
+      sahkoposti: "aku.ankka@ankkalinna.com",
+      muistiinpanoja: "Kattivaaran margariinitehtaalla töissä.",
+      lisatietoja: "Ei halua lisätietoja."
+    }
+  ];
+
+  function paivitaTaulukko() {
+    var str='';
+    for(var i=0;i<henkilot.length;i++) {
+      str += '<tr>';
+      str += '<td>' + henkilot[i].etunimi + '</td>';
+      str += '<td>' + henkilot[i].sukunimi + '</td>';
+      str += '<td>' + henkilot[i].sahkoposti + '</td>';
+      str += '<td>' + henkilot[i].muistiinpanoja + '</td>';
+      str += '<td>' + henkilot[i].lisatietoja + '</td>';
+      str += '</tr>';
+    }
+    $('tbody').html(str);
+  }
+
   function tyhjenna() {
     $('#etunimi').val('');
     $('#sukunimi').val('');
@@ -7,10 +31,21 @@ $(function() {
   }
 
   $('form#henkilotiedot').submit(function(e) {
+    var henkilo = {};
+    henkilo.etunimi = $('#etunimi').val();
+    henkilo.sukunimi = $('#sukunimi').val();
+    henkilo.sahkoposti = $('#sahkoposti').val();
+    henkilo.muistiinpanoja = $('#muistiinpanoja').val();
+    henkilo.lisatietoja = $('#lisatietoja').val();
+    henkilot.push(henkilo);
+    paivitaTaulukko();
+    tyhjenna();
     e.preventDefault();
   });
 
   $('button#tyhjenna').click(function(e) {
     tyhjenna();
   });
+
+  paivitaTaulukko();
 });
